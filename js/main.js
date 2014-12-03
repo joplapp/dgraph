b87ad16e22acc6e1afd25b5d03f28a50961833e3
Johannes Plapp <johannes@plapp.de>
2014-12-02 20:20:27 -0500

    v3

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Johannes on 02.12.2014.
+ */
+
+
+var client = new Dropbox.Client({ key: "your-key-here" });
+
+function authenticate(){
+    client.authenticate(function(error, client) {
+        if (error) {
+            // Replace with a call to your own error-handling code.
+            //
+            // Don't forget to return from the callback, so you don't execute the code
+            // that assumes everything went well.
+            return showError(error);
+        }
+
+        // Replace with a call to your own application code.
+        //
+        // The user authorized your app, and everything went well.
+        // client is a Dropbox.Client instance that you can use to make API calls.
+        doSomethingCool(client);
+    });
+}
